2918c3fe26050b8513698a77147ac44c9635a8a8
Сергей Горячев <serezha_goryachev@list.ru>
2021-07-08 11:42:31 +0400

    исправил токен

--- App_Airport/App_Airport/Services/APIManager.m
+++ App_Airport/App_Airport/Services/APIManager.m
@@ -9,7 +9,7 @@
 #import "DataManager.h"
 #import "Ticket.h"
 
-#define API_TOKEN @"0fa373e04e1c63743565bf8e13cc1486"
+#define API_TOKEN @"b21d05f52ad8a21dc099d266eaa869d9"
 #define API_URL_IP_ADDRESS @"https://api.ipify.org/?format=json"
 #define API_URL_CHEAP @"https://api.travelpayouts.com/v1/prices/cheap"
 #define API_URL_CITY_FROM_IP @"https://www.travelpayouts.com/whereami?ip="
