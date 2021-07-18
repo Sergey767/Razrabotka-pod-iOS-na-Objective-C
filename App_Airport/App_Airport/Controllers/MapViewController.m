a1f2eed383d7bc3c4a5de50d05814b7aad14092d
Сергей Горячев <serezha_goryachev@list.ru>
2021-07-18 17:12:17 +0400

    Урок 4. Работа с картами и определение местоположения

---
+++ App_Airport/App_Airport/Controllers/MapViewController.m
@@ -0,0 +1,97 @@
+//
+//  MapViewController.m
+//  App_Airport
+//
+//  Created by Сергей Горячев on 14.07.2021.
+//
+
+#import "MapViewController.h"
+#import "LocationService.h"
+#import "APIManager.h"
+#import <MapKit/MapKit.h>
+#import "DataManager.h"
+#import "MapPrice.h"
+#import <CoreLocation/CoreLocation.h>
+
+@interface MapViewController ()
+
+@property (strong, nonatomic) MKMapView *mapView;
+@property (nonatomic, strong) LocationService *locationService;
+@property (nonatomic, strong) City *origin;
+@property (nonatomic, strong) NSArray *prices;
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"Карта цен";
+    
+    _mapView = [[MKMapView alloc] initWithFrame:self.view.bounds];
+    _mapView.showsUserLocation = YES;
+    [self.view addSubview:_mapView];
+    
+    [[DataManager sharedInstance] loadData];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(dataLoadedSuccessfully) name:kDataManagerLoadDataDidComplete object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateCurrentLocation:) name:kLocationServiceDidUpdateCurrentLocation object:nil];
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)dataLoadedSuccessfully {
+    _locationService = [[LocationService alloc] init];
+}
+
+- (void)updateCurrentLocation:(NSNotification *)notification {
+    CLLocation *currentLocation = notification.object;
+    
+    [self addressFromLocation:currentLocation];
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(currentLocation.coordinate, 1000000, 1000000);
+    [_mapView setRegion: region animated: YES];
+    
+    if (currentLocation) {
+        _origin = [[DataManager sharedInstance] cityForLocation:currentLocation];
+        if (_origin) {
+            [[APIManager sharedInstance] mapPricesFor:_origin withCompletion:^(NSArray *prices) {
+                self.prices = prices;
+            }];
+        }
+    }
+}
+
+-(void)addressFromLocation:(CLLocation *)location {
+    
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+            
+            if ([placemarks count] > 0) {
+                for (MKPlacemark *placemark in placemarks) {
+                    NSLog(@"%@", placemark.name);
+                }
+            }
+    }];
+}
+
+- (void)setPrices:(NSArray *)prices {
+    _prices = prices;
+    [_mapView removeAnnotations: _mapView.annotations];
+ 
+    for (MapPrice *price in prices) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+            annotation.title = [NSString stringWithFormat:@"%@ (%@)", price.destination.name, price.destination.code];
+            annotation.subtitle = [NSString stringWithFormat:@"%ld руб.", (long)price.value];
+            annotation.coordinate = price.destination.coordinate;
+            [self->_mapView addAnnotation: annotation];
+        });
+    }
+}
+
+@end
---
+++ App_Airport/App_Airport/Services/LocationService.h
@@ -0,0 +1,17 @@
+//
+//  LocationService.h
+//  App_Airport
+//
+//  Created by Сергей Горячев on 14.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+#define kLocationServiceDidUpdateCurrentLocation @"LocationServiceDidUpdateCurrentLocation"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocationService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
